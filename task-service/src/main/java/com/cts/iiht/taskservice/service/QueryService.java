34822b031b45eb76c4b5ef2b749cbe2129907cfb
anoop31jan <anoop31jan@gmail.com>
2023-05-10 16:33:26 +0530

    Query handler completed-commiit -task service-2

---
+++ task-service/src/main/java/com/cts/iiht/taskservice/service/QueryService.java
@@ -0,0 +1,39 @@
+package com.cts.iiht.taskservice.service;
+
+import com.cts.iiht.taskservice.entity.*;
+import com.cts.iiht.taskservice.external.*;
+import com.cts.iiht.taskservice.external.client.*;
+import com.cts.iiht.taskservice.helper.*;
+import com.cts.iiht.taskservice.model.*;
+import com.cts.iiht.taskservice.repository.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.*;
+
+import java.util.*;
+import java.util.stream.*;
+
+@Service
+public class QueryService {
+
+    @Autowired
+    TaskRepository taskRepository;
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    TaskServiceHelper taskServiceHelper;
+
+
+    public List<TaskDetailsDto> getListOfTaskDetails(String memberId) {
+
+        List<Task> taskList = taskRepository.getTaskBymemberId(memberId);
+
+        ProjectMemberClient projectMemberClient = memberService.getMemberDetails(memberId);
+
+        return taskList.stream()
+                .map(task -> taskServiceHelper.prepareTaskDtoObject(task, projectMemberClient))
+                .collect(Collectors.toList());
+
+    }
+}
